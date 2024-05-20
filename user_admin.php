2b18050613539c63caeb8e58e0bc65a0009c810b
Rabi Roy <rrabir9815@gmail.com>
2024-05-20 12:51:12 +0545

    changed

---
+++ user_admin.php
@@ -0,0 +1,16 @@
+<?php include('layouts/header.php') ?>
+
+
+     <!-- Contact -->
+     <section id="contact" class="container my-5 py-5">
+        <div class="container text-center mt-5">
+        <h3 style="color: #fb774b;">Login page</h3>
+      <a href="./admins/admin_login.php" class="btn btn-primary">Admin Login</a>
+      <a href="/login.php" class="btn btn-success ">User Login</a> <!-- Adjusted margin top for spacing -->
+      </div>
+     </section>
+
+       <!-- footer -->
+<?php include('layouts/footer.php') ?>
+
+
